ba31c297fc6bb63bd7a73be453583940e2542c87
Дмитрий Скурихин <dmitry.skurihin@monopoly.su>
2019-04-30 16:39:37 +0300

    init commit

---
+++ test-file.js
@@ -0,0 +1,12 @@
+import some from 'some';
+import { test } from 'test';
+
+export class TestClass extends React.Component {
+
+  public render(): React.ReactNode {
+
+    return (
+      <div />
+    );
+  }
+}
