7f26c92ef3a508476d92d8c903b5c4cc8fb279e1
Дмитрий Скурихин <dmitry.skurihin@monopoly.su>
2019-05-31 18:36:54 +0300

    fix propTypes pattern

--- script.hs
+++ script.hs
@@ -21,21 +21,24 @@ replace :: FilePath -> IO ()
 replace path = do
   isJsx <- isJsxFile path
   inplace (firstRoundPattern isJsx) path
-  inplace secondRoundPattern path
+  inplace (secondRoundPattern isJsx) path
     where
       firstRoundPattern isJsx = if isIndexFile path
         then exportDefaultFromPattern
         else if isJsx then jsxFilePatterns else tsPatterns
-      secondRoundPattern = propTypesPattern <|> exportDefaultPattern
+      secondRoundPattern isJsx = if isJsx then propTypesPattern <|> exportDefaultPattern else exportDefaultPattern
       tsPatterns = tslintDisabledPattern <|> jsImportPattern
       jsxFilePatterns = tsPatterns <|> genericPattern
       genericPattern = "Component {" *> pure "Component<any, any> {"
-      exportDefaultPattern = "export default " *> pure "export "
+      exportDefaultPattern = begins ("export default " *> pure "export ")
       tslintDisabledPattern = "export " *> pure "/* tslint:disable */\nexport "
       jsImportPattern = ends (".js';" *> pure "';")
       exportDefaultFromPattern = begins ("export { default }" *> pure "export *")
       propTypesPattern =
-        (begins "static propTypes = {" *> pure "") <|> (ends "};" *> pure "") <|> (contains "PropTypes" *> pure "")
+        ((spaces1 *> contains "propTypes" *> star anyChar *> newline)
+        -- *> (star (spaces1 *> (star anyChar) *> "PropTypes" *> (star anyChar) *> newline))
+        -- *> (spaces1 *> "};" *> newline)
+        ) *> pure ""
 
 rename :: FilePath -> IO ()
 rename path = do
@@ -50,14 +53,19 @@ rename path = do
 findJsPaths :: IO [FilePath]
 findJsPaths = collectFilePaths $ find (ends "js") "."
 
+findTsPaths :: IO [FilePath]
+findTsPaths = collectFilePaths $ find (ends "ts" <|> ends "tsx") "."
+
 jsPathsFromFile :: IO [FilePath]
 jsPathsFromFile = collectFilePaths $ (fromText . lineToText) <$> input "./paths.txt"
 
 main :: IO [()]
 main = do
-  cd "/Users/dmitry.skurihin/Projects/MonopolyOnline.Frontend/frontend/src/layouts"
+  cd "/Users/dmitry.skurihin/Projects/MonopolyOnline.Frontend/frontend/src/widgets/AlertModal/components"
   paths <- findJsPaths
+  tsPaths <- findTsPaths
   -- paths <- jsPathsFromFile
   
   traverse replace paths
+  traverse replace tsPaths
   traverse rename paths
