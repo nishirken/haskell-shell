f1d97aa625db1a5a7cc9bc61bae8ecc7d0df997d
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-09-09 23:00:52 +0300

    completed test for Replace imports

--- test/ReplaceDefaultImportsSpec/ReplaceSpec.hs
+++ test/ReplaceDefaultImportsSpec/ReplaceSpec.hs
@@ -7,10 +7,12 @@ import Test.Hspec (describe, it, Spec, shouldBe, runIO)
 import ReplaceDefaultImports.Replace (replaceDefaultImports)
 import System.FilePath.Posix ((</>), addExtension, dropExtension, takeDirectory, takeExtension, isExtensionOf)
 import qualified System.IO.Strict as StrictIO
+import qualified Turtle
 import TestUtils
 import qualified Data.Text as T
-import Control.Monad (forM)
+import Control.Monad (forM, forM_)
 import System.Directory (getDirectoryContents)
+import ProcessPaths (collectFilePaths)
 
 initialFolder = basePath </> "ChangeUser"
 
@@ -22,28 +24,28 @@ data TestFile = TestFile
 
 makeTestFile :: FilePath -> IO TestFile
 makeTestFile originalPath = do
-  print $ "PATH: " <> originalPath
   originalContent <- T.pack <$> StrictIO.readFile originalPath
-  expectContent <- T.pack <$> StrictIO.readFile (addExtension (takeExtension originalPath)
-    $ dropExtension originalPath </> ".expect")
+  let expectFileWithoutExt = dropExtension originalPath <> ".expect"
+  expectContent <- T.pack <$> StrictIO.readFile
+    (addExtension expectFileWithoutExt (takeExtension originalPath))
   pure $ TestFile originalContent expectContent originalPath
 
 recoverFile :: TestFile -> IO ()
 recoverFile TestFile{..} = writeFile _path $ T.unpack _originalContent
 
-testOnFile :: TestFile -> IO ()
+testOnFile :: TestFile -> Spec
 testOnFile TestFile{..} =
-  _originalContent `shouldBe` _expectContent
+  it _path $ _originalContent `shouldBe` _expectContent
 
 replaceSpec :: Spec
 replaceSpec = describe "Replace" $ do
   testPaths <- runIO (do
-    testPaths <- getDirectoryContents initialFolder
+    let turtleShellPaths = Turtle.find (Turtle.has ".ts") $ Turtle.decodeString initialFolder
+    testPaths <- map Turtle.encodeString <$> collectFilePaths turtleShellPaths
     let
       originalPaths :: [FilePath]
       originalPaths = filter
         (\path -> (isExtensionOf "ts" path || isExtensionOf "tsx" path) && (not . T.isSuffixOf ".expect" . T.pack . dropExtension) path) testPaths
-    print originalPaths
     forM originalPaths makeTestFile)
-  it "index" $ do
-    testOnFile $ testPaths !! 0
+  runIO $ replaceDefaultImports $ map (\TestFile{..} -> Turtle.decodeString _path) testPaths
+  forM_ testPaths testOnFile
