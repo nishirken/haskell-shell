e0a119a59c8dc90e94c87a9bf9ced243533d44fa
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-09-10 22:13:25 +0300

    start ResolveImports test

--- test/ReplaceDefaultImportsSpec/ResolveImportsSpec.hs
+++ test/ReplaceDefaultImportsSpec/ResolveImportsSpec.hs
@@ -2,25 +2,52 @@
 
 module ReplaceDefaultImportsSpec.ResolveImportsSpec where
 
-import Test.Hspec (describe, it, shouldBe, Spec, runIO)
+import Test.Hspec (describe, it, shouldBe, Spec, runIO, context)
 import TestUtils (baseTestFilesPath)
 import ReplaceDefaultImports.Replace (resolveAndMakeAbsoluteImportPath)
 import qualified Turtle
+import System.FilePath.Posix ((</>))
 import ProcessPaths (getProjectPath)
+import ReplaceDefaultImportsSpec.ReplaceSpec (initialFolder)
+
+expectPath :: FilePath -> Turtle.FilePath
+expectPath path = Turtle.decodeString $ initialFolder </> path
+
+turtleInitialFolder = Turtle.decodeString initialFolder
 
 resolveImportsSpec :: Spec
 resolveImportsSpec = describe "ResolveImports" $ do
-  projectPath <- runIO (do
-    c <- Turtle.pwd
-    pure c)
+  projectPath <- runIO $ Turtle.pwd
   runIO $ Turtle.cd $ Turtle.decodeString baseTestFilesPath
 
-  it "two paths are absolute" $ do
-    path <- resolveAndMakeAbsoluteImportPath "rootFolder/someFile" "rootFolder/anotherFolder/someFile"
-    path `shouldBe` "rootFolder/anotherFolder/someFile"
+  context "two paths are absolute" $ do
+    it "containers index" $ do
+      path <- resolveAndMakeAbsoluteImportPath turtleInitialFolder "containers"
+      path `shouldBe` expectPath "containers/index.ts"
 
-  it "currentPath is absolute and importPath is relative" $ do
-    path <- resolveAndMakeAbsoluteImportPath "rootFolder/someFile" ""
-    path `shouldBe` ""
+    it "containers ChangeUserState" $ do
+      path <- resolveAndMakeAbsoluteImportPath turtleInitialFolder "containers/ChangeUserState"
+      path `shouldBe` initialFolder </> "containers/ChangeUserState.ts"
+    
+    it "containers ChangeUserContainer" $ do
+      path <- resolveAndMakeAbsoluteImportPath turtleInitialFolder "containers/ChangeUserContainer"
+      path `shouldBe` initialFolder </> "containers/ChangeUserContainer.ts"
+
+    it "components ChangeUserForm" $ do
+      path <- resolveAndMakeAbsoluteImportPath turtleInitialFolder "components/ChangeUserForm/ChangeUserForm"
+      path `shouldBe` initialFolder </> "components/ChangeUserForm/ChangeUserForm.tsx"
+
+    it "components index" $ do
+      path <- resolveAndMakeAbsoluteImportPath turtleInitialFolder "components/ChangeUserForm"
+      path `shouldBe` initialFolder </> "components/ChangeUserForm/index.ts"
+
+  context "currentPath is absolute and importPath is relative" $ do
+    it "containers ChangeUserContainer" $ do
+      path <- resolveAndMakeAbsoluteImportPath (turtleInitialFolder </> "containers") "./ChangeUserContainer"
+      path `shouldBe` initialFolder </> "containers/ChangeUserContainer.ts"
+
+    it "components ChangeUserForm" $ do
+      path <- resolveAndMakeAbsoluteImportPath (turtleInitialFolder </> "components") "./ChangeUserForm/ChangeUserForm"
+      path `shouldBe` initialFolder </> "components/ChangeUserForm.tsx"
 
   runIO $ Turtle.cd projectPath
